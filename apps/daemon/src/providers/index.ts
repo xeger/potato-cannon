ebf5edf9c1705b1b65a84fe356c4171d3af84c23
Tony Spataro <xeger@xeger.net>
2026-03-01 21:39:59 -0800

    feat: export Slack provider from providers index

--- apps/daemon/src/providers/index.ts
+++ apps/daemon/src/providers/index.ts
@@ -4,3 +4,6 @@ export * from './chat-provider.types.js';
 export { TelegramProvider } from './telegram/telegram.provider.js';
 export { TelegramApi } from './telegram/telegram.api.js';
 export { TelegramPoller } from './telegram/telegram.poller.js';
+export { SlackProvider } from './slack/slack.provider.js';
+export { SlackApi } from './slack/slack.api.js';
+export { SlackSocket } from './slack/slack.socket.js';
